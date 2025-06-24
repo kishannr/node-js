5a676f27680e0d0def3c80da21935459bb1159ef
kishannr <kishornr12345678@gmail.com>
2025-06-24 15:35:58 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.46.90',
+  serverUrl: 'http://13.232.14.126:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
