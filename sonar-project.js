0c222f774e00b7fd4ad7c7ab12a6515b6e695326
kishannr <kishornr12345678@gmail.com>
2025-06-26 13:57:02 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,17 +1,15 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.205.198.62:9000/',
+  serverUrl: 'http://<your-sonarqube-ip>:9000',
   options: {
-    'sonar.projectDescription': 'This is a Node JS application',
-    'sonar.projectName': 'Node JS Application - Sample',
     'sonar.projectKey': 'NodeJsMSS',
-    'sonar.login': 'squ_5d6e2f8cc29ebaefd6b44a7e696b0f539e993f48',
+    'sonar.projectName': 'Node JS Application - Sample',
+    'sonar.projectDescription': 'This is a Node JS application',
     'sonar.projectVersion': '1.0',
+    'sonar.login': 'your_sonar_token_here',
     'sonar.language': 'js',
     'sonar.sourceEncoding': 'UTF-8',
-    'sonar.sources': '.',
-    // 'sonar.tests': 'specs',
-    // 'sonar.inclusions': 'src/**'
+    'sonar.sources': '.'
   },
 }, () => {});
